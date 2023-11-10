c39d94984dc212ae896c562c5e0fc222025f9a18
marcosray <131471350+marcosray@users.noreply.github.com>
2023-11-10 13:13:57 +0100

    Update diabetesapp.py

--- diabetesapp.py
+++ diabetesapp.py
@@ -1,27 +1,17 @@
-import subprocess
-import joblib
-
-# Explicitly install dependencies
-subprocess.call(['pip', 'install', 'scikit-learn==0.24.2', 'joblib==1.0.1'])
-
 import streamlit as st
 import pandas as pd
 import os
+import joblib
 
-subprocess.call(['pip', 'install', 'scikit-learn==0.24.2', 'joblib==1.0.1'])
+#subprocess.call(['pip', 'install', 'scikit-learn==0.24.2', 'joblib==1.0.1'])
 
 
 # Load the trained model
 #model_path = os.path.join(os.path.dirname(__file__), 'diabetes_model.joblib')
 
 # Load the model
-#model = joblib.load('diabetes_model.joblib')
-model_path = 'diabetes_model.joblib'
+model = joblib.load('diabetes_model.joblib')
 
-if not os.path.exists(model_path):
-    raise FileNotFoundError(f"Model file not found at {model_path}. Please check the file path.")
-
-model = joblib.load(model_path)
 
 # Streamlit app description
 st.write("""
